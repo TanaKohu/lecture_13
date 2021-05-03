d760f8b4a91831d8a682d323b7a1e7ed167ba19a
TanaKohu <tanakohu@gmail.com>
2021-05-03 10:37:23 +0200

    fibo:

--- fibo.py
+++ fibo.py
@@ -1,6 +1,19 @@
 
+
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+    if n == 1:
+        return n
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
 def main():
-    pass
+    n = 3
+
+    output = recursive_nth_fibo(n)
+    print(output)
 
 
 if __name__ == '__main__':
